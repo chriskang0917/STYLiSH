11c5bd6d60f3d1f2ede0fc332ea24fd1f9fb9a73
Chris <chriskang0917@gmail.com>
2023-11-03 23:45:35 +0800

    Chore: Add test ui

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
     plugins: [react()],
+    server: {
+      open: true,
+    },
   };
 });
