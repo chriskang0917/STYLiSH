86b219b708b312ebbd9b336dd130a2545c853df3
Chris <chriskang0917@gmail.com>
2023-11-04 16:42:27 +0800

    Refactor: Rename socket api

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,26 +3,9 @@ import { io } from "socket.io-client";
 const host = "http://13.55.105.122:3000/";
 const socketIO = io(host);
 
-/* Socket usage
-
-onSnapshot: Put onSnapshot in useEffect to listen to new messages
-socket.onSnapshot((data) => {
-  setMessages((prevMessages) => [...prevMessages, data]);
-});
-
-send: Put send in onSubmit handler to send a message
-socket.send(input);
-setMessages((prevMessages) => [...prevMessages, input]);
-
-isAdmin: Call isAdmin before onSnapshot to change identity to admin
-socket.isAdmin();
-socket.onSnapshot()...
-
-*/
-
 export const socket = {
   isUser: true,
-  onSnapshot(setMessage) {
+  receive(setMessage) {
     socketIO.on("talk", (message) => {
       setMessage(message);
     });
