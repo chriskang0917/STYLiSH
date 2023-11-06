b797aa8e7015ed5668f29e59f05de3f12a2af2c6
Chris <chriskang0917@gmail.com>
2023-11-06 10:24:13 +0800

    Doc: Update socket.js for user-check message

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,7 +6,7 @@ export class Socket {
     this.jwtToken = localStorage.getItem("jwtToken") || undefined;
   }
 
-  connect(identity) {
+  connect(identity = "user") {
     this.user = identity;
 
     const userIdentity = [identity, this.jwtToken];
@@ -17,14 +17,13 @@ export class Socket {
     this.socket.on("talk", (message) => {
       setMessage(message);
     });
+    this.socket.on("user-check", (user) => {
+      console.log("user-check: ", user);
+    });
   }
 
   send(message) {
     const userIdentity = [this.user, this.jwtToken];
     this.socket.emit("talk", message, userIdentity);
-  }
-
-  disconnect() {
-    this.socket.disconnect();
   }
 }
