af8ef6a034206b1bd6600bb29f55bd3c32838792
Chris <chriskang0917@gmail.com>
2023-11-08 11:45:40 +0800

    Fix: Display alert when not selecting color and size on mobile

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -313,13 +313,13 @@ function Product() {
       setStock(selectShopStocks[0].shopStocks);
     }
     getStock();
-    console.log(stock);
-    console.log(mapTargetProduct);
   }, [id, mapTargetProduct]);
-  console.log(stock);
+
   if (!product) return null;
   if (!stock) return null;
-  const hasShopDetail = true;
+
+  const hasShopDetail = stock.length > 0;
+
   return (
     <Wrapper>
       <MainImage src={`https://handsomelai.shop${product.main_image}`} />
@@ -347,7 +347,7 @@ function Product() {
           <GoogleMap mapTargetProduct={mapTargetProduct} />
         </MapContainer>
 
-        {hasShopDetail &&
+        {hasShopDetail ? (
           stock.map((shopStock) => (
             <ShopDetails>
               <ShopDetail key={shopStock.lat}>
@@ -364,7 +364,10 @@ function Product() {
                 </ShopStockWrapper>
               </ShopDetail>
             </ShopDetails>
-          ))}
+          ))
+        ) : (
+          <p>請先選擇顏色與尺寸。</p>
+        )}
       </GoogleMapContainer>
       <Story>
         <StoryTitle>細部說明</StoryTitle>
